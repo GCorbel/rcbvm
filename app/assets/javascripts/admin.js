06b7154e8a8fd3c54f9bbd043dd9694f9e98c87d
gcorbel <guirec.corbel@gmail.com>
2012-11-14 17:51:15 -0500

    show javascript for admin only for administrators

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,8 @@
+//= require underscore
+//= require backbone
+//
+//= require .//rcbvm
+//
+//= require_tree ../templates/
+//= require_tree .//models
+//= require_tree .//views
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,11 +14,3 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require aloha-config
-//= require underscore
-//= require backbone
-//
-//= require .//rcbvm
-//
-//= require_tree ../templates/
-//= require_tree .//models
-//= require_tree .//views
