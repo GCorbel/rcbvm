b12cda578ce35e8f179fa0a69daebbe2e220761a
Guirec Corbel <guirec.corbel@gmail.com>
2013-03-31 12:19:53 -0400

    change default locale

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,13 +1,13 @@
 # encoding: utf-8
 
 Refinery::I18n.configure do |config|
-  # config.enabled = true
+  config.enabled = true
 
-  # config.default_locale = :en
+  config.default_locale = :fr
 
   # config.current_locale = :en
 
-  # config.default_frontend_locale = :en
+  config.default_frontend_locale = :fr
 
   # config.frontend_locales = [:en]
 
