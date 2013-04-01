46e0d07ab0246189d9fad98d4694072c797f5f11
Guirec Corbel <guirec.corbel@gmail.com>
2013-03-31 22:09:29 -0400

    change the sites name in refinery

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -14,7 +14,7 @@ Refinery::Core.configure do |config|
   # config.base_cache_key = :refinery
 
   # Site name
-  config.site_name = "RCBVM"
+  config.site_name = "Base-De-Moisie"
 
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
